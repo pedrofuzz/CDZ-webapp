d1dfe1cf08584d401aa455dae7a37268f55e341d
Pedro <pedrofuzario@hotmail.com>
2022-02-04 14:15:28 -0400

    Delete CDZ directory

--- CDZ/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    if(isset($_SESSION['email'])) {
-        header('Location: /cdz/views/home.php');
-    } else {
-        header('Location: /cdz/views/login.php');
-    }
-?>
